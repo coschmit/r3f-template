b9658d940dd139b5692287ea6e1b61155f4a491e
colinshmt <colinschmitt47@gmail.com>
2022-04-10 22:34:56 -0400

    Add Suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 import CanvasScene from "./components/CanvasScene";
 
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [0, 0, 2] }}>
-        <CanvasScene />
-      </Canvas>
+      <Suspense fallback={null}>
+        <Canvas camera={{ position: [0, 0, 2] }}>
+          <CanvasScene />
+        </Canvas>
+      </Suspense>
     </>
   );
 }
